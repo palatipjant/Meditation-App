5d82788dd1ed7075090020f3cfaab9c0fb356f67
Palatip Jantawong <palatipjant@outlook.com>
2023-10-16 16:18:48 +0700

    Initial Commit

---
+++ Meditation-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meditation-App
+//
+//  Created by Palatip Jantawong on 16/10/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meditation-App/Meditation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_AppApp.swift
+//  Meditation-App
+//
+//  Created by Palatip Jantawong on 16/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
